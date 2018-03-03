6ab6621dbff18cb727fc125c375f3b39548aa2c7
Caleb Lewis <caleb_dre@yahoo.com>
2018-03-03 04:42:38 -0500

    more graphs

--- rk2.py
+++ rk2.py
@@ -3,16 +3,16 @@ from util import generate_iterations
 
 def rk2(template, f, h, bounds, iv, solution_f):
 	start, stop = bounds
-	iteration_values = generate_iterations(start, h, stop)
+	iteration_values = generate_iterations(start+h, h, stop+h)
 	estimate = [iv]
 	solution = [iv]
 
-	for i, t in enumerate(iteration_values):
+	for i, t in enumerate(iteration_values, 1):
 		w = estimate[i-1]
-		calc = w + h * f(t + (h/2), w + ((h/2) * f(t, w)))
+		calc = w + h * f(t-h + (h/2), w + ((h/2) * f(t-h, w)))
 		solu = solution_f(t)
 
-		print("val: {}, exact: {}, error: {}".format(round(calc, 5), round(solu,5), round(solu - calc, 5)))
+		print("ti = {} , val: {}, exact: {}, error: {}".format(t, round(calc, 5), round(solu,5), round(solu - calc, 5)))
 		
 		estimate.append(calc)
 		solution.append(solu)
